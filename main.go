8c56e4d6ec49b5a0d6a40052f002cc839dd8190e
pamonu <57515389+pamonu@users.noreply.github.com>
2019-11-08 15:15:52 +1100

    Test change

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Hello World Changed Test"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
